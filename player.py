25b0dfed01f7b0735ee6aa663ec960e16c6899f1
Mark <80956787+MGorbl@users.noreply.github.com>
2021-06-20 19:01:08 +0300

    Add files via upload

--- player.py
+++ player.py
@@ -11,6 +11,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(r"ball.png")
         self.rect = self.image.get_rect()
         self.onGround = False
+        self.jump = False
+        self.jumpcount = 30
 
         self.rect.x = 150
 
@@ -26,3 +28,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 5
         elif key[pygame.K_d]:
             self.rect.x += 5
+
+        #jump
+        if key[pygame.K_SPACE] and self.onGround == True:
+            self.jump = True
+
+        if self.jump:
+            self.PlayerJump()
+
+    def PlayerJump(self):
+        if self.jumpcount >= -30:
+            if self.jumpcount < 0:
+                self.rect.y += self.jumpcount
+            self.rect.y -= self.jumpcount
+            self.jumpcount -= 1.5
+        else:
+            self.jumpcount = 30
+            self.jump = False
