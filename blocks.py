f29406fd4cf8fde206aa7cf9c4261558b561e436
Mark <80956787+MGorbl@users.noreply.github.com>
2021-06-21 18:03:43 +0300

    Add files via upload

--- blocks.py
+++ blocks.py
@@ -3,15 +3,15 @@ from settings import *
 
 class Block(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, color, coordinates):
         super().__init__()
 
-        self.image = pygame.Surface((400, 30))
-        self.image.fill((255, 255, 255))
+        self.image = pygame.Surface((800, 30))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
 
-        self.rect.y = 600
-        self.rect.x = 100
+        self.rect.y = coordinates[1]
+        self.rect.x = coordinates[0]
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
